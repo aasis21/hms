6b875a1fa7f27f064963ef3f35156317dcb1fef6
akashks1998 <akashks@iitk.ac.in>
2019-04-11 01:02:56 +0530

    Some changes

--- accounts/profile_context.py
+++ accounts/profile_context.py
@@ -1,4 +1,4 @@
-from .models import Profile
+from .models import Profile,Post
 from django.core.exceptions import ObjectDoesNotExist
 
 
@@ -6,9 +6,14 @@ def context(request):
     if request.user.is_authenticated:
         try:
             profile = Profile.objects.get(user=request.user)
-            return {'profile': profile}
+            post=Post.objects.filter(user=request.user)
+            if post.exists():
+                post=post.first()
+            else:
+                post=0
+            return {'profile': profile,'post':post}
         except ObjectDoesNotExist:
-            return {'profile': 0}
+            return {'profile': 0,'post':0}
 
     else:
-        return {'profile': 0}
+        return {'profile': 0,'post':0}
--- bill/urls.py
+++ bill/urls.py
@@ -6,9 +6,9 @@ app_name = 'bills'
 
 urlpatterns = [
     path('', views.bill, name='bill'),
-    path('addbill', views.addbill, name='addbill'),
-    path('transiction', views.transiction, name='transiction'),
-    path('rmremb/<int:id>', views.rmremb,name='rmremb'),
-    path('messrem',views.mess_rembursement,name='messrem'),
-    path('accept',views.accept,name='accept')
+    path('addbill/', views.addbill, name='addbill'),
+    path('transiction/', views.transiction, name='transiction'),
+    path('rmremb/<int:id>/', views.rmremb,name='rmremb'),
+    path('messrem/',views.mess_rembursement,name='messrem'),
+    path('accept/',views.accept,name='accept')
 ]
