a5550e46bb10c2e73f25c0a44cf9058e57a8db94
akashks1998 <akashks@iitk.ac.in>
2019-04-10 17:50:41 +0530

    Some changes in layout

--- hms/settings.py
+++ hms/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.profile_context.context',
+                'bill.bill_context.context',
             ],
         },
     },
