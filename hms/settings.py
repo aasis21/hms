ed731f3db72ce0806295c2e132e0f3794ca45ebe
Ashish Kumar <ak.ashish2108@gmail.com>
2019-04-24 21:49:54 +0530

    updated readme

--- hms/settings.py
+++ hms/settings.py
@@ -130,7 +130,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hms.hall12.iitk@gmail.com'
-EMAIL_HOST_PASSWORD = 'hall12@hms'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD' ,'hall12@hms')
 DEFAULT_FROM_EMAIL = 'hms.hall12.iitk@gmail.com'
 
 
